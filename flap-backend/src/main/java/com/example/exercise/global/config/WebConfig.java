fddcb74ac7cf9c04dbe7d19b45525d34d63c1e33
eunoia73 <soo97703@naver.com>
2026-03-19 14:26:35 +0900

    docker

--- flap-backend/src/main/java/com/example/exercise/global/config/WebConfig.java
+++ flap-backend/src/main/java/com/example/exercise/global/config/WebConfig.java
@@ -10,7 +10,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "http://3.36.73.116:5173",
+                        "http://3.36.73.116"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
